f82151d5f4e5773b6136bcc1101d87fc892cc11b
riko19 <gr0348032018@unab.edu.sv>
2020-11-28 14:50:49 -0600

    primerPrueba

---
+++ apoloClinic/src/main/java/com/prograiv/apoloClinic/ApoloClinicApplication.java
@@ -0,0 +1,12 @@
+package com.prograiv.apoloClinic;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApoloClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApoloClinicApplication.class, args);
+	}
+
+}
